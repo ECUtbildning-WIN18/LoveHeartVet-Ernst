c526cf7c4de7d2a9d3a9b410ea0782efeff39c83
ernestonr1 <ernstras@gmail.com>
2018-10-23 12:30:01 +0200

    Add project files.

---
+++ LH-Vet.0.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LH_Vet._0._1.MvcApplication" Language="C#" %>
---
+++ LH-Vet.0.1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LH_Vet._0._1.Startup))]
+namespace LH_Vet._0._1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
